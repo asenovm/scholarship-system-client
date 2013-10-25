1a8f81c8d1a714eede7c1784f62d220974de8503
martin <asenov.m@gmail.com>
2013-10-25 16:21:39 +0300

    * Setup an angularjs app

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('scholarshipSystemClientApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
