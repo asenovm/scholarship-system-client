475bc07e01d48083ba543d08e8b13148f5e96e8a
martin <asenov.m@gmail.com>
2013-10-25 22:32:08 +0300

    * The registration view is pretty much up and running

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,7 +1,9 @@
-
 'use strict';
 
 angular.module('scholarshipSystemClientApp')
   .controller('RegisterController', function ($scope) {
-
+    $('.selectpicker').selectpicker({
+        width: 460,
+        dropupAuto: false
+    });
   });
