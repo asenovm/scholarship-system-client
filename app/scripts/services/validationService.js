ade05605e3b8f98ca455dff34f5f079557349a92
martin <asenov.m@gmail.com>
2013-10-25 22:39:46 +0300

    * Added a service to be used for validating the user-entered content

---
+++ app/scripts/services/validationService.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('scholarshipSystemClientApp')
+    .service('ValidationService', function () {
+        
+        this.validateEmail = function (email) {
+            
+        };
+
+        this.validatePassword = function (password) {
+        
+        };
+
+        this.validateName = function (name) {
+        
+        };
+
+        this.validateFaculityId = function (id) {
+        
+        };
+    });
