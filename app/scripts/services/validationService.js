c2495cd3339f796222e5334e1fada18efd01969f
martin <asenov.m@gmail.com>
2013-10-25 22:51:45 +0300

    * Added some functionality to the validation service, fiddled with the registration form

--- app/scripts/services/validationService.js
+++ app/scripts/services/validationService.js
@@ -4,18 +4,18 @@ angular.module('scholarshipSystemClientApp')
     .service('ValidationService', function () {
         
         this.validateEmail = function (email) {
-            
+            return /.+@.+\..+/.test(email);           
         };
 
         this.validatePassword = function (password) {
-        
+            return password.length >= 8 && password.search(/[A-Z]/) >= 0;       
         };
 
         this.validateName = function (name) {
-        
+            return /[a-zA-Z]+/.test(name);
         };
 
         this.validateFaculityId = function (id) {
-        
+            return /[0-9]+/.test(id);
         };
     });
